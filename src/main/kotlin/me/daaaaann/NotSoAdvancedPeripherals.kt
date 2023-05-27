016e75499b7cce9feb44859f78b2778ddb42612c
DAAAAANN <134781578+DAAAAANN@users.noreply.github.com>
2023-05-27 16:12:46 +0100

    Initial commit

---
+++ src/main/kotlin/me/daaaaann/NotSoAdvancedPeripherals.kt
@@ -0,0 +1,13 @@
+package me.daaaaann
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object NotSoAdvancedPeripherals : ModInitializer {
+    private val logger = LoggerFactory.getLogger("Not So Advanced Peripherals")
+
+    override fun onInitialize() {
+        // This code runs as soon as Minecraft is in a mod-load-ready state.
+        logger.info("Initialised")
+    }
+}
