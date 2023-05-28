ed4f6bc2bd14a8147494776acf2b444271dc72dd
DAAAAANN <134781578+DAAAAANN@users.noreply.github.com>
2023-05-28 19:14:20 +0100

    Integrated Reborn energy API

--- src/main/kotlin/me/daaaaann/EnergyPeripheral.kt
+++ src/main/kotlin/me/daaaaann/EnergyPeripheral.kt
@@ -4,16 +4,20 @@ import dan200.computercraft.api.lua.LuaFunction
 import dan200.computercraft.api.lua.MethodResult
 import dan200.computercraft.api.peripheral.IPeripheral
 import net.minecraft.util.math.BlockPos
+import net.minecraft.util.math.Direction
 import net.minecraft.world.World
+import team.reborn.energy.api.EnergyStorage
 
 class EnergyPeripheral : IPeripheral {
 
     private final val world: World
     private final val pos: BlockPos
+    private final val side: Direction
 
-    constructor(world: World, pos: BlockPos) {
+    constructor(world: World, pos: BlockPos, side: Direction) {
         this.world = world
         this.pos = pos
+        this.side = side
     }
 
     override fun equals(iPeripheral: IPeripheral?): Boolean {
@@ -21,11 +25,16 @@ class EnergyPeripheral : IPeripheral {
     }
 
     override fun getType(): String {
-        return "energy"
+        return "energy_storage"
     }
 
-    @LuaFunction
-    fun test(): MethodResult {
-        return MethodResult.of(true, "Wow it worked!")
+    @LuaFunction(mainThread = true)
+    fun getEnergy(): MethodResult {
+        return MethodResult.of(EnergyStorage.SIDED.find(world, pos, side)?.amount)
+    }
+
+    @LuaFunction(mainThread = true)
+    fun getEnergyCapacity(): MethodResult {
+        return MethodResult.of(EnergyStorage.SIDED.find(world, pos, side)?.capacity)
     }
 }
--- src/main/kotlin/me/daaaaann/EnergyPeripheralProvider.kt
+++ src/main/kotlin/me/daaaaann/EnergyPeripheralProvider.kt
@@ -2,19 +2,30 @@ package me.daaaaann
 
 import dan200.computercraft.api.peripheral.IPeripheral
 import dan200.computercraft.api.peripheral.IPeripheralProvider
+import dan200.computercraft.shared.peripheral.generic.GenericPeripheralProvider
 import net.minecraft.block.entity.BlockEntity
 import net.minecraft.util.math.BlockPos
 import net.minecraft.util.math.Direction
 import net.minecraft.world.World
+import org.slf4j.LoggerFactory
+import team.reborn.energy.api.EnergyStorage
 import techreborn.blockentity.machine.GenericMachineBlockEntity
+import techreborn.blockentity.storage.energy.EnergyStorageBlockEntity
 import techreborn.init.TRContent.MachineBlocks
 
 class EnergyPeripheralProvider : IPeripheralProvider {
-    override fun getPeripheral(world: World, pos: BlockPos, side: Direction): IPeripheral? {
-        val block: BlockEntity? = world.getBlockEntity(pos)
 
-        if (block is GenericMachineBlockEntity) {
-            return EnergyPeripheral(world, pos)
+    private val logger = LoggerFactory.getLogger("Not So Advanced Peripherals")
+
+    override fun getPeripheral(world: World, pos: BlockPos, side: Direction): IPeripheral? {
+
+        val energyStorage: EnergyStorage? = EnergyStorage.SIDED.find(world, pos, side)
+
+        energyStorage.let {
+            if (energyStorage!!.supportsInsertion() || energyStorage.supportsExtraction()) {
+                logger.info("Found one!")
+                return EnergyPeripheral(world, pos, side)
+            }
         }
 
         return null;
